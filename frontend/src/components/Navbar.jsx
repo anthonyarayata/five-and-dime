7403679fb27260b7c614d4edec1722f54752bc05
anthonyarayata <arayataanthonyross@icloud.com>
2024-07-21 07:23:18 +0800

    updated feature section for mobile view, navbar changes

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -12,8 +12,10 @@ import {
 } from "./ui/navigation-menu";
 import { motion } from "framer-motion";
 import { Link } from 'react-router-dom';
-import logo from '../assets/images/logoblack.png';
-import smlogo from '../assets/images/smlogoblack.png';
+import logo from '../assets/images/logowhite.png';
+import smlogo from '../assets/images/smlogowhite.png';
+import logoblack from '../assets/images/logoblack.png';
+import smlogoblack from '../assets/images/smlogoblack.png';
 
 const Navbar = () => {
     
@@ -56,14 +58,16 @@ const Navbar = () => {
                 initial={{ height: 0, opacity: 0 }}
                 animate={{
                 height: isScrolled || isHovered ? "100%" : 0,
-                opacity: isScrolled || isHovered ? .8 : 0,
+                opacity: isScrolled || isHovered ? 1 : 0,
                 }}
                 transition={{ duration: 0.2, ease: [0, 0.2, 0.5, 1] }}
             />
-            <div className="grid grid-cols-4 py-1 font-iconaSans">
+            <div className={`grid grid-cols-4 py-1 font-iconaSans text-white ${isScrolled || isHovered ? 'text-black' : ''}`}>
                 <div className="col-span-1 grid place-items-start items-center">
-                    <img src={logo} className="hidden md:block relative h-8 px-4"/>
-                    <img src={smlogo} className="block md:hidden relative h-8 px-4"/>
+                    <img src={isScrolled || isHovered ? logoblack : logo} 
+                        className="hidden md:block relative h-8 px-4"/>
+                    <img src={isScrolled || isHovered ? smlogoblack : smlogo} 
+                        className="block md:hidden relative h-8 px-4"/>
                 </div>
                 <div className="col-span-2 grid place-items-center text-sm md:text-lg">
                     <NavigationMenu>
--- frontend/src/scenes/Feature.jsx
+++ frontend/src/scenes/Feature.jsx
@@ -5,30 +5,34 @@ const Feature = () => {
     return (
         <>
         <PageWrapper>
-            <div className='grid grid-cols-11 grid-rows-9 h-screen max-w-full gap-3'>
-
-                {/* Desktop View */}
+            {/* Desktop View */}
+            <div className='hidden md:grid grid-cols-11 grid-rows-9 h-screen max-w-full gap-3'>
                 <div className='col-span-full row-span-1 flex justify-center items-end md:pr-6 text-2xl'>
                     <h1 className='font-drukwidebold'>Feature ahh section</h1>
                 </div>
-                <div className='col-span-5 row-span-4 hidden md:block bg-slate-100'>
+                <div className='col-span-5 row-span-4  bg-slate-100'>
 
                 </div>
-                <div className='col-span-3 row-span-4 hidden md:block bg-slate-200'>
+                <div className='col-span-3 row-span-4  bg-slate-200'>
 
                 </div>
-                <div className='col-span-3 row-span-8 hidden md:block bg-slate-300'>
+                <div className='col-span-3 row-span-8  bg-slate-300'>
 
                 </div>
-                <div className='col-span-3 row-span-4 hidden md:block bg-slate-400'>
+                <div className='col-span-3 row-span-4  bg-slate-400'>
 
                 </div>
-                <div className='col-span-5 row-span-4 hidden md:block bg-slate-500'>
+                <div className='col-span-5 row-span-4  bg-slate-500'>
 
                 </div>
+            </div>
 
-                {/* Mobile Carousel */}
-                <div>
+            {/* Mobile Carousel */}
+            <div className='md:hidden grid grid-cols-6 grid-rows-9 h-screen max-w-full gap-3'>
+                <div className='col-span-full row-span-1 flex justify-center items-end md:pr-6 text-2xl'>
+                    <h1 className='font-drukwidebold'>Feature ahh section</h1>
+                </div>
+                <div className='col-span-4 col-start-2 row-span-4 row-start-3 bg-black'>
 
                 </div>
             </div>
