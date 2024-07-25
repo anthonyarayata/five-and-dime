610dce14d13eb8680e8b0727585861456c14407e
anthonyarayata <arayataanthonyross@icloud.com>
2024-07-25 16:38:46 +0800

    styled footer for mobile (WIP)

--- frontend/src/scenes/Footer.jsx
+++ frontend/src/scenes/Footer.jsx
@@ -8,8 +8,8 @@ import InstagramIcon from '@mui/icons-material/Instagram';
 const Footer = () => {
     return (
         <div className='pt-8 bg-zinc-950 text-white'>
-            <div className="grid grid-cols-3 grid-rows-3 gap-2">
-                <div className='px-4 col-span-1 row-span-1 row-start-1 grid grid-rows-3'>
+            <div className="grid grid-cols-2 md:grid-cols-3 grid-rows-3 gap-2">
+                <div className='px-4 col-span-full row-span-1 row-start-1 grid grid-rows-3'>
                     <h4 className="text-sm font-drukwidebold leading-none row-span-1 row-start-1">Five and Dime</h4>
                     <p className="text-xs text-muted-foreground font-iconaSans row-span-1 row-start-3">
                         A Filipino clothing brand.
@@ -18,7 +18,8 @@ const Footer = () => {
             
                 <Separator className="col-span-full row-span-1 row-start-2 self-center" />
 
-                <div className="px-4 col-span-full row-span-1 row-start-3 grid grid-cols-2">
+                {/*Desktop Layout*/}
+                <div className="hidden px-4 col-span-full row-span-1 row-start-3 md:grid grid-cols-2">
                     <div className='col-span-1 col-start-1 grid grid-cols-2'>
                         <div className='col-span-1 grid grid-cols-9 text-sm'>
                             <div className='col-span-1'>
@@ -38,13 +39,39 @@ const Footer = () => {
                             </div>
                         </div>
                     </div>
-                    <div className='col-span-1 col-start-2 flex justify-end items-center text-xs font-iconaSans'>
+                    <div className='col-span-1 col-start-2 flex justify-end items-center text-xs font-iconaSans text-muted-foreground'>
                         <span>Website created by:</span>
                         <a href="https://www.linkedin.com/in/anthony-ross-arayata-4195a4195/" target="_blank" rel="noopener noreferrer" className="underline ml-2">
                             Anthony Ross Arayata
                         </a>
                     </div>
                 </div>
+
+                {/*Mobile Layout*/}
+                <div className='md:hidden col-span-full row-start-3 px-4 pb-4 grid grid-cols-3 grid-rows-3'>
+                    <div className='col-span-1 flex justify-center items-center'>
+                        <a href="https://www.facebook.com/fiveanddime.cvt" target="_blank" rel="noopener noreferrer">
+                            <FacebookIcon className='text-xl' />
+                        </a>
+                    </div>
+                    <div className='col-span-1 flex justify-center items-center'>
+                        <a href="https://www.instagram.com/fiveanddime.cvt/" target="_blank" rel="noopener noreferrer">
+                            <InstagramIcon className='text-xl' />
+                        </a>
+                    </div>
+                    <div className='col-span-1 flex justify-center items-center'>
+                        <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer">
+                            <XIcon className='text-xl'/>
+                        </a>
+                    </div>
+                    <div className='col-span-full row-span-1 row-start-3 flex justify-center items-center text-xs font-iconaSans text-muted-foreground'>
+                        <span>Website created by:</span>
+                        <a href="https://www.linkedin.com/in/anthony-ross-arayata-4195a4195/" target="_blank" rel="noopener noreferrer" className="underline ml-2">
+                            Anthony Ross Arayata
+                        </a>
+                    </div>
+                </div>
+
             </div>
         </div>
     );
